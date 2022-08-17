740304a468d84147325549721d4b5aa77e667f19
Andre-souza539 <aljs.junca@gmail.com>
2022-08-17 15:19:32 -0300

    atualizacao17082022

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Oie Ana <link href="#">Next.js!</link>
+          Oie Ana <a href="#">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
